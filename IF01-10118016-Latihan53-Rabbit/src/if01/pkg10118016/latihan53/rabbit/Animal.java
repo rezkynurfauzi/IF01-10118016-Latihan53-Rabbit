4d28670f57a98327e5cbad3bd36cc5b76188070d
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-11-05 09:52:18 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan53-Rabbit/src/if01/pkg10118016/latihan53/rabbit/Animal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan53.rabbit;
+
+/**
+ *
+ * @author Rezky
+ */
+public class Animal {
+    protected boolean vegetarian;
+    protected String eats;
+    protected int noOfLegs;
+
+    public Animal(boolean vegetarian, String food, int legs) {
+        this.vegetarian = vegetarian;
+        this.eats = food;
+        this.noOfLegs = legs;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public String getEats() {
+        return eats;
+    }
+
+    public int getNoOfLegs() {
+        return noOfLegs;
+    }
+
+}
---
+++ IF01-10118016-Latihan53-Rabbit/src/if01/pkg10118016/latihan53/rabbit/IF0110118016Latihan53Rabbit.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan53.rabbit;
+
+/**
+ *
+ * @author Rezky
+ * NAMA      : Rezky Nur Fauzi
+ * KELAS     : IF-1
+ * NIM       : 10118016
+ * Deskripsi Program   : Program ini berisi program Rabbit
+ */
+
+public class IF0110118016Latihan53Rabbit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Rabbit rabbit = new Rabbit("Peter",false, "grass", 4, "grey" );
+        System.out.println("Hello, His name is "+rabbit.getName());
+        System.out.println(rabbit.getName()+" is Vegetarian? "+rabbit.isVegetarian());
+        System.out.println(rabbit.getName()+" eats "+rabbit.getEats());
+        System.out.println(rabbit.getName()+" has "+rabbit.getNoOfLegs()+" legs.");
+        System.out.println(rabbit.getName()+" color is "+rabbit.getColor());
+    }
+    
+}
